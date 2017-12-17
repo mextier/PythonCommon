a95fb95fc450c3fd7401edf880a3d928d32764f5
mextier <34535363+mextier@users.noreply.github.com>
2017-12-17 13:35:00 +0600

    1
    1

--- mssqltest.py
+++ mssqltest.py
@@ -2,7 +2,7 @@ import pymssql
 
 
 
-conn = pymssql.connect(host='192.168.0.2', user='Администратор', password='as2010', database='ClarisTKHP', as_dict=True)
+conn = pymssql.connect(host='192.168.0.2', user='Администратор', password='', database='ClarisTKHP', as_dict=True)
 cur = conn.cursor()
 
 cur.execute('SELECT * FROM glo WHERE cod=%d', 1)

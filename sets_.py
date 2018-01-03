13503368aa412ce7efcfdf9a638223948c322fce
mextier <mextier@gmail.com>
2018-01-04 01:23:25 +0600

    sets

--- sets_.py
+++ sets_.py
@@ -19,3 +19,11 @@ print('newlist is {}'.format(newlist))
 
 newset = set(range(1,11))
 print(newset)
+
+
+fib_numbers = set([1, 1, 2, 3, 5, 8, 13, 21])
+prime_numbers = set([2, 3, 5, 7, 11, 13, 17, 19])
+union = fib_numbers | prime_numbers
+intersection = fib_numbers & prime_numbers
+difference = fib_numbers - prime_numbers
+symmetric_difference = fib_numbers ^ prime_numbers

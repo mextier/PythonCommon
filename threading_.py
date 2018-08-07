ecb1ade945c02a3300d92fe7ccf7019f8195db61
mextier <34535363+mextier@users.noreply.github.com>
2018-08-07 13:00:22 +0600

    1

--- threading_.py
+++ threading_.py
@@ -1,2 +1,17 @@
 #https://www.youtube.com/watch?v=3lzfxUN8BUI&index=34&list=PLfAlku7WMht6janxhS4D7XqajI7Knq1sS
 #https://www.youtube.com/playlist?list=PLV5rPVRE79YikyqHCIrJ33ttjgsXluLVB
+#https://docs.python.org/2/library/threading.html
+
+
+import threading
+import time
+
+
+def proca(name):
+    time.sleep(10)
+    print(f'{name} finished')
+
+for i in range(10):
+    t = threading.Thread(target=proca,name=f't{i}',args=(f'thread{i}',))
+    t.start()
+print('continue...')

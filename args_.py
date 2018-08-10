3b6840509ab86ef3563a6258dc2e46a35e22f55a
mextier <34535363+mextier@users.noreply.github.com>
2018-08-10 16:56:58 +0600

    1

--- args_.py
+++ args_.py
@@ -1,16 +1,39 @@
 def cla1(*args):
-    for p in args:
-        print(p)
-    pass
-
+    for i,p in enumerate(args):
+        print(i,p)
 
 def cla2(**kwargs):
     print(kwargs)
-    pass
-
 
 
 cla1(1,10,20,30)
+print('*'*50)
+cla1(1,*"abcd")
+print('*'*50)
+cla1(1,*[5,6,1,2,18])
+print('*'*50)
 
 cla2(param1 =1 , request =2, data={})
-#cla2({"param1":10, "request":20})
+print('*'*50)
+cla2(text="as")
+print('*'*50)
+
+
+
+
+d = {}
+param1=10
+param2=20
+
+
+
+d["param1"]=1
+d["param2"]=2
+#cla1(**d)
+breakpoint()
+
+
+
+a=10
+b=20
+print("{a} {b}".format(**locals()))

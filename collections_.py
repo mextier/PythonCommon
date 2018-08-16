aa72bb65d9bff5a6354ba03cf79ad3193476ee6f
mextier <34535363+mextier@users.noreply.github.com>
2018-08-16 12:43:46 +0600

    1

--- collections_.py
+++ collections_.py
@@ -1,10 +1,17 @@
 #https://docs.python.org/3.3/library/collections.html
 #https://pythonworld.ru/moduli/modul-collections.html
+#https://www.youtube.com/watch?v=_RxuKNR3VeA
+#https://www.youtube.com/watch?v=CZWJT1tm-8g
 
-import collections
+from collections import OrderedDict, Counter
 
 dict1 = dict()
 dict1['Filed2']="value2"
 dict1['Field1']="value1"
-dict2=collections.OrderedDict(sorted(dict1.items()))
+dict2=OrderedDict(sorted(dict1.items()))
 print(dict2)
+
+
+#from collections import Counter
+d = [10,20,30,30,40,50,50,50]
+print(Counter(d))

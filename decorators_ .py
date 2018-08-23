ab9e860ba86f5f971e5c65a76b2da129b77c713d
mextier <34535363+mextier@users.noreply.github.com>
2018-08-23 17:03:43 +0600

    1

--- decorators_ .py
+++ decorators_ .py
@@ -1,1 +1,2 @@
 #https://lancelote.gitbooks.io/intermediate-python/content/book/decorators.html
+#https://www.youtube.com/watch?v=Ss1M32pp5Ew&list=PLlWXhlUMyooYqypXIju-5czBtppKaWimP&index=6
--- pandas_.py
+++ pandas_.py
@@ -1,1 +1,4 @@
 #https://khashtamov.com/ru/pandas-introduction/
+#https://pythonworld.ru/obrabotka-dannyx/pandas-cookbook-0-ipython.html
+#https://pythonspot.com/category/pandas/
+#https://devpractice.ru/pandas-start-part1/

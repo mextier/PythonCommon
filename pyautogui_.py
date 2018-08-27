45dff3e0f9982c10d043da1d6fb32135fd74c74f
mextier <34535363+mextier@users.noreply.github.com>
2018-08-27 16:17:25 +0600

    1

--- pyautogui_.py
+++ pyautogui_.py
@@ -1,1 +1,20 @@
 #http://pyautogui.readthedocs.io/en/latest/
+
+
+import pyautogui
+
+screenWidth, screenHeight = pyautogui.size()
+
+#pyautogui.FAILSAFE = False # disables the fail-safe
+pyautogui.PAUSE = 1
+#pyautogui.moveTo(100, 100)
+pyautogui.moveRel(10,10)
+pyautogui.moveRel(10,10)
+pyautogui.moveRel(10,10)
+#pyautogui.alert(text='text here', title='title', button='OK')
+pjyautogui.press('enter')
+pyautogui.press('enter')
+
+
+
+#pyautogui.click()

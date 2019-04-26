c7b7b83e5eaa203c14d8aa82ccb7b5d94f9e4ff5
mextier <mextier@gmail.com>
2019-04-26 23:04:21 +0600

    time_

---
+++ time_.py
@@ -0,0 +1,14 @@
+import time
+
+
+
+break_time = int(input("Length of break (seconds):"))
+work_time = int(input("Length of working time (seconds):"))
+start_time = time.time()
+print("Clock has been started!")
+while True:
+    if start_time+work_time <= time.time():
+        print("{} You need to take a break!".format(time.ctime()))
+        start_time = time.time()
+    time.sleep(1)
+    

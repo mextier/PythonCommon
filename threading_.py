f9adaa0aaf236fc2e3a22e0dca4098175c843215
mextier <mextier@gmail.com>
2019-05-11 11:37:58 +0600

    threading

--- threading_.py
+++ threading_.py
@@ -1,6 +1,7 @@
 #https://www.youtube.com/watch?v=3lzfxUN8BUI&index=34&list=PLfAlku7WMht6janxhS4D7XqajI7Knq1sS
 #https://www.youtube.com/playlist?list=PLV5rPVRE79YikyqHCIrJ33ttjgsXluLVB
 #https://docs.python.org/2/library/threading.html
+#https://www.youtube.com/watch?v=kKl7scEW90c
 
 
 import threading
